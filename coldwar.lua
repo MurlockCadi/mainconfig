c46399e35b0f23d6eae61d6a28efec07bdf4db96
bullfroghogam <tamerhosny000@web.de>
2025-01-14 01:02:42 +0100

    GDR/FGR/USSR/WARSAWPACT/NATO Change
    check if its good!!!!

--- coldwar.lua
+++ coldwar.lua
@@ -42,8 +42,8 @@ group_ranks = {
 
 
 DivisionTypes = {
-    ["Commonwealth Forces"] = {
-        Team = "allies",
+    ["UK Royal Infantry Division"] = {
+        Team = "NATO",
         Ranks = {
             {ID = 1, Name = "Recruit", Rank_Prefix = "RCT", ModelsAllowed = BC_MODELS, WeaponsAllowed = {}},
             {ID = 2, Name = "Private", Rank_Prefix = "PVT", ModelsAllowed = BC_MODELS, WeaponsAllowed = {}},
@@ -61,7 +61,7 @@ DivisionTypes = {
         },
         Classes = {
             {
-                Name = "Rifleman",
+                Name = "British Rifleman",
                 Default = true,
                 Models = {},
                 Weapons = {},
@@ -70,8 +70,8 @@ DivisionTypes = {
             },
         },
     },
-    ["U.S Paratroopers"] = {
-        Team = "allies",
+    ["US Armored Cavalry Division"] = {
+        Team = "NATO",
         Ranks = {
             {ID = 1, Name = "Recruit", Rank_Prefix = "RCT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
             {ID = 2, Name = "Private", Rank_Prefix = "PVT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
@@ -89,7 +89,7 @@ DivisionTypes = {
         },
         Classes = {
             {
-                Name = "Rifleman",
+                Name = "General Infantry Rifleman",
                 Default = true,
                 Models = {},
                 Weapons = {},
@@ -98,26 +98,82 @@ DivisionTypes = {
             },
         },
     },
-    ["Wehrmacht"] = {
-        Team = "axis",
+        ["FGR Panzergrenadier Division"] = {
+        Team = "NATO",
         Ranks = {
-            {ID = 1, Name = "Soldat", Rank_Prefix = "SDT", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 2, Name = "Gefreiter", Rank_Prefix = "GFR", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 3, Name = "Obergefreiter", Rank_Prefix = "OGF", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 4, Name = "Unteroffizier", Rank_Prefix = "UO", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 5, Name = "Feldwebel", Rank_Prefix = "FW", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 6, Name = "Oberfeldwebel", Rank_Prefix = "OFW", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 7, Name = "Hauptfeldwebel", Rank_Prefix = "HFW", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 8, Name = "Leutnant", Rank_Prefix = "LT", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 9, Name = "Oberleutnant", Rank_Prefix = "OLT", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 10, Name = "Hauptmann", Rank_Prefix = "HPT", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 11, Name = "Major", Rank_Prefix = "MAJ", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 12, Name = "Oberstleutnant", Rank_Prefix = "OLT", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
-            {ID = 13, Name = "Colonel", Rank_Prefix = "COL", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 1, Name = "Schütze", Rank_Prefix = "SOL", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 2, Name = "Gefreiter", Rank_Prefix = "GFR", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 3, Name = "Obergrefreiter", Rank_Prefix = "OGRF", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 4, Name = "Unteroffizier", Rank_Prefix = "UFFZ", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 5, Name = "Stabsunteroffizier", Rank_Prefix = "STABU", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 6, Name = "Feldwebel", Rank_Prefix = "FW", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 7, Name = "Oberfeldwebel", Rank_Prefix = "OFW", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 8, Name = "Leutnant", Rank_Prefix = "LT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 9, Name = "Oberleutnant", Rank_Prefix = "OLT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 10, Name = "Hauptmann", Rank_Prefix = "HPT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 11, Name = "Major", Rank_Prefix = "MAJ", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 12, Name = "Oberstleutnant", Rank_Prefix = "OBLT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 13, Name = "Oberst der BRD", Rank_Prefix = "OBT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
         },
         Classes = {
             {
-                Name = "Soldat",
+                Name = "Panzergrenadier",
+                Default = true,
+                Models = {},
+                Weapons = {},
+                SpawnWeapons = {},
+                Prefix = ""
+            },
+        },
+    },
+            ["GDR Motorisierte Schützen"] = {
+        Team = "Warsaw Pact",
+        Ranks = {
+            {ID = 1, Name = "Schütze", Rank_Prefix = "SOL", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 2, Name = "Gefreiter", Rank_Prefix = "GFR", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 3, Name = "Obergrefreiter", Rank_Prefix = "OGRF", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 4, Name = "Unteroffizier", Rank_Prefix = "UFFZ", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 5, Name = "Stabsunteroffizier", Rank_Prefix = "STABU", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 6, Name = "Feldwebel", Rank_Prefix = "FW", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 7, Name = "Oberfeldwebel", Rank_Prefix = "OFW", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 8, Name = "Leutnant", Rank_Prefix = "LT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 9, Name = "Oberleutnant", Rank_Prefix = "OLT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 10, Name = "Hauptmann", Rank_Prefix = "HPT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 11, Name = "Major", Rank_Prefix = "MAJ", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 12, Name = "Oberstleutnant", Rank_Prefix = "OBLT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+            {ID = 13, Name = "Oberst der DDR", Rank_Prefix = "OBT", ModelsAllowed = AIRBORN_MODELS, WeaponsAllowed = {}},
+        },
+        Classes = {
+            {
+                Name = "Panzergrenadier",
+                Default = true,
+                Models = {},
+                Weapons = {},
+                SpawnWeapons = {},
+                Prefix = ""
+            },
+        },
+    },
+    ["Soviet Motorized Rifle Division"] = {   
+        Team = "Warsaw Pact",
+        Ranks = {
+            {ID = 1, Name = "Novobranets", Rank_Prefix = "NOV", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}}, -- I could make it that the names are the respective language, and that the Prefix stays american yk like RCT etc.
+            {ID = 2, Name = "Riadovoy", Rank_Prefix = "RIA", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 3, Name = "Yefreytor", Rank_Prefix = "YEF", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 4, Name = "Mladshiy Serzhant", Rank_Prefix = "MLS", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 5, Name = "Serzhant", Rank_Prefix = "SER", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 6, Name = "Starshiy Serzhant", Rank_Prefix = "SST", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 7, Name = "Starshina", Rank_Prefix = "SAR", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 8, Name = "Mladshiy Leytenant", Rank_Prefix = "MLY", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 9, Name = "Leytenant", Rank_Prefix = "LEY", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 10, Name = "Kapitan", Rank_Prefix = "KAP", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 11, Name = "Mayor", Rank_Prefix = "MAJ", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 12, Name = "Podpolkovnik", Rank_Prefix = "POD", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+            {ID = 13, Name = "Polkovnik", Rank_Prefix = "POL", ModelsAllowed = DE_ENLISTED_MODELS, WeaponsAllowed = {}},
+        },
+        Classes = {
+            {
+                Name = "солдат/ Motozired Rifleman",
                 Default = true,
                 Models = {},
                 Weapons = {},
@@ -159,8 +215,8 @@ ROLES = ROLES or {
         staff = true
     },
     DIVISION_ALLIES = {
-        NiceName = "Allied Recruits",
-        Prefix = "ALLIED",
+        NiceName = "NATO Recruits",
+        Prefix = "NATO",
         Color = Color(75, 97, 47, 255),
         Description = "Enlist in the A.E.F and help Liberate Europe from the Nazi.\n Your task will not be an easy one. Your enemy is well trained, well equipped and battle-hardened.\n He will fight savagely.",
         Default = true,
@@ -184,11 +240,11 @@ ROLES = ROLES or {
                 Prefix = ""
             },
         },
-        Team = "allies"
+        Team = "NATO"
     },
     DIVISION_AXIS = {
-        NiceName = "Axis Recruits",
-        Prefix = "AXIS",
+        NiceName = "Warsaw Pact Recruits",
+        Prefix = "Warsaw Pact",
         Color = Color(70, 80, 90, 255),
         Description = "Conscripted into the Wermarcht you have orders to defend the Reich against Allied Forces.\n If we can't throw the enemy into the sea within twenty-four hours then that will be.\n the beginning of the end.",
         Default = true,
@@ -212,7 +268,7 @@ ROLES = ROLES or {
                 Prefix = ""
             },
         },
-        Team = "axis"
+        Team = "Warsaw Pact"
     },
 }
 DEFAULT_ROLE = ROLES.DIVISION_ALLIES
