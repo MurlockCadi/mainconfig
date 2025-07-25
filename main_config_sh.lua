5c5433288ee0b76a9f3499b9463c07bd1eadbb6a
Matty Cooke <87860752+MurlockCadi@users.noreply.github.com>
2025-07-25 10:47:34 +0100

    Update main_config_sh.lua

--- main_config_sh.lua
+++ main_config_sh.lua
@@ -1208,23 +1208,28 @@ DivisionEnhancements = {
 ----------- INVENTORY CONFIGURATION ---------------
 
 INVENTORY_ITEMS = {
-    1 = {
+    [1] = {
         Name = "Scrap Crate",
         Description = "Crate filled with Scrap. Used in Scrap Factories to produce Credits.",
         Model = "models/dizcordum/wh/dot_props/crate_ammo.mdl",
         Category = "Scrap",
         Entity = "outpost_supply",
-        UseFunction = Example(ply)
+        UseFunction = function(ply)
+            Example(ply)
+        end
     },
-    2 = {
+    [2] = {
         Name = "Spook",
         Description = "A mysterious substance created by the Chem Runners. Addictive Narcotic.",
         Model = "models/jackswan/wk_pills_two.mdl",
         Category = "Drugs",
         Entity = "drugs",
-        UseFunction = Example(ply)
+        UseFunction = function(ply)
+            Example(ply)
+        end
     }
 }
+
 
 
 -----------------LOAD MODELS------------------
