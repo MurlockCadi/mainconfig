ab440f39f31029a67eeb4accfb9285c4add513c8
Lordswagalot93 <Lordswagalot93@gmail.com>
2025-11-08 20:24:54 -0500

    Update main_config_sh.lua

--- main_config_sh.lua
+++ main_config_sh.lua
@@ -822,18 +822,18 @@ DivisionTypes = {
         Ranks = {
             {ID = 1, Name = "Penal Conscript", Rank_Prefix = "", ModelsAllowed = PENAL_MODELS, WeaponsAllowed = PENAL_WEAPONS, CanPromote = false}
         },
-        Classes = {
-            {
-                Name = "Penal Conscript",
-                Default = true,
-                Core = true,
-                Models = {},
-                Weapons = {},
-                SpawnWeapons = {},
-                Prefix = "Conscript"
-            }
-	}
-},
+    Classes = {
+        {
+            Name = "Penal Conscript",
+            Default = true,
+            Core = true,
+            Models = {},
+            Weapons = {},
+            SpawnWeapons = {},
+            Prefix = "Conscript"
+        }
+    }
+}
     },
     ["Auxiliary"] = {
         NiceName = "Auxiliary",
