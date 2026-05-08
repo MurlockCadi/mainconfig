5cecd29d0b25b6eb56861cfdfe364f283531e487
Lordswagalot93 <Lordswagalot93@gmail.com>
2026-05-08 05:57:08 -0400

    Update forge_construct_weapons.lua

--- warhammer_misc_configs/forge_construct_weapons.lua
+++ warhammer_misc_configs/forge_construct_weapons.lua
@@ -97,7 +97,8 @@ CRAFTABLE_ITEMS["item_285839"] = 20000 -- force halberd
 CRAFTABLE_ITEMS["item_319275"] = 15000 -- Power maul
 CRAFTABLE_ITEMS["item_121044"] = 15000 -- power greatsword
 
-
+CRAFTABLE_ITEMS["item_506025"] = 30000 -- Heavy Flamer
+CRAFTABLE_ITEMS["item_881186"] = 30000 -- Ogryn Mace + shield
 
 CRAFTABLE_ITEMS["item_214114"] = 20000 -- Chainsword
 CRAFTABLE_ITEMS["item_889608"] = 20000 -- Ogryn Club
