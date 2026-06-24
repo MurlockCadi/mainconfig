dedbb657ec786b0e315fccaf82624fba25475eb8
Lordswagalot93 <Lordswagalot93@gmail.com>
2026-06-24 02:34:42 -0400

    Add Human attribute to GOBLINS bot

--- warhammer_faction_types/GOBLINS.lua
+++ warhammer_faction_types/GOBLINS.lua
@@ -205,6 +205,7 @@ FACTION_TYPES["GOBLINS"] = {
                 bot:SetNWBool("ShieldEnable", true)
                 bot:SetNWString("Description", "A cunning gretchin that leads the others.")
                 health = math.max(health * 5, 1000)
+                Human = true
             end
         end
 
