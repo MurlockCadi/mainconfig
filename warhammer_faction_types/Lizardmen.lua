0a8261fb6a0a21e25154e3860c84c0d2ed768d9b
Matty Cooke <87860752+MurlockCadi@users.noreply.github.com>
2026-08-09 15:25:37 +0100

    Update Lizardmen.lua

--- warhammer_faction_types/Lizardmen.lua
+++ warhammer_faction_types/Lizardmen.lua
@@ -3,7 +3,7 @@ FACTION_TYPES["Lizardmen"] = {
     DeathsByType = {},
     OffWorld = true,
 
-    CreateLizardman = function(self, bot, elite)
+    CreateBot = function(self, bot, elite)
         local health = GetHealthByTotalPlayers()
         local spawn_elite = elite or math.random(1, 100) <= 5
 
@@ -181,7 +181,7 @@ FACTION_TYPES["Lizardmen"] = {
         if BOT_INVASION ~= "Lizardmen" then return end
         for _, bot in ipairs(player.GetBots()) do
             if bot:GetNWString("Status") ~= "LIZARDMEN" then
-                self:CreateLizardman(bot, ALWAYS_SPAWN_ELITES)
+                self:CreateBot(bot, ALWAYS_SPAWN_ELITES)
             end
         end
     end,
