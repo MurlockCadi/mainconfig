3b0518b5dbce95b58a4a918ef510bc24641bebc9
Matty Cooke <87860752+MurlockCadi@users.noreply.github.com>
2026-08-09 15:26:08 +0100

    Update Mirror Image.lua

--- warhammer_faction_types/Mirror Image.lua
+++ warhammer_faction_types/Mirror Image.lua
@@ -3,7 +3,7 @@ FACTION_TYPES["Mirror Image"] = {
     DeathsByType = {},
     OffWorld = true,
 
-    CreateTraitorGuardsmen = function(self, bot, elite)
+    CreateBot = function(self, bot, elite)
         local TotalPlayers = #player.GetHumans()
         local health = GetHealthByTotalPlayers()
         local spawn_elite = elite or math.random(1, 100) <= 5
@@ -89,7 +89,7 @@ FACTION_TYPES["Mirror Image"] = {
         if BOT_INVASION ~= "Mirror Image" then return end
         for _, bot in ipairs(player.GetBots()) do
             if bot:GetNWString("Status") ~= "TRAITOR_GUARDSMEN" then
-                self:CreateTraitorGuardsmen(bot, ALWAYS_SPAWN_ELITES)
+                self:CreateBot(bot, ALWAYS_SPAWN_ELITES)
             end
         end
     end,
