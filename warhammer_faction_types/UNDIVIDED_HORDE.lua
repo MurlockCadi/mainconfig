7a7c94b1ce7dfa523414f3a013779c5534632ba1
Matty Cooke <87860752+MurlockCadi@users.noreply.github.com>
2026-08-09 15:30:21 +0100

    Update UNDIVIDED_HORDE.lua

--- warhammer_faction_types/UNDIVIDED_HORDE.lua
+++ warhammer_faction_types/UNDIVIDED_HORDE.lua
@@ -3,7 +3,7 @@ FACTION_TYPES["UNDIVIDED_HORDE"] = {
     DeathsByType = {},
     OffWorld = true,
 
-    CreateUndividedUnit = function(self, bot, elite)
+    CreateBot = function(self, bot, elite)
         local health = GetHealthByTotalPlayers()
 
         local spawn_elite = elite or math.random(1, 100) <= 25
@@ -207,7 +207,7 @@ FACTION_TYPES["UNDIVIDED_HORDE"] = {
         if BOT_INVASION ~= "UNDIVIDED_HORDE" then return end
         for _, bot in ipairs(player.GetBots()) do
             if bot:GetNWString("Status", "friendly") ~= "UNDIVIDED_HORDE" then
-                self:CreateUndividedUnit(bot, ALWAYS_SPAWN_ELITES)
+                self:CreateBot(bot, ALWAYS_SPAWN_ELITES)
             end
         end
     end,
