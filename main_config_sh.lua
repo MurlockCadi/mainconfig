def12897bb58d5a77b650f43547e2097293fb2bb
Matty Cooke <87860752+MurlockCadi@users.noreply.github.com>
2026-08-10 03:52:38 +0100

    Update main_config_sh.lua

--- main_config_sh.lua
+++ main_config_sh.lua
@@ -467,7 +467,9 @@ commands = {
 	["!gmqueue"]   = function(ply) return handleGMQueueCommand(ply)   end,
 	["/rphistory"] = function(ply) return handleRPHistoryCommand(ply) end,
 	["!rphistory"] = function(ply) return handleRPHistoryCommand(ply) end,
-	["/spawn_items"] = function(ply, args) return AdminSpawnItem(ply, args) end
+	["/spawn_items"] = function(ply, args) return AdminSpawnItem(ply, args) end,
+    ["/civbots"] = function(ply) AdminBotOpenMenu(ply) end,
+    ["!civbots"] = function(ply) AdminBotOpenMenu(ply) end,
 }
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
