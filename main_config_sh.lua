cd1bbddb805c4c8079ecad409e86c0c5ccdea172
genotaletime <genotaletime@gmail.com>
2026-09-24 17:14:26 +0100

    Update main_config_sh.lua

--- main_config_sh.lua
+++ main_config_sh.lua
@@ -1758,25 +1758,6 @@ DivisionTypes = {
                     },
                 },
             },
-			{
-                Name = "Contracted Mercenary Medicae", 
-                Default = false, 
-				Core = true,
-				Medic = true,
-                Models = MERC_MODELS, 
-                Weapons = {}, 
-                SpawnWeapons = {"fas2_ifak","ent_spawner","refinery_spawner","weapon_fists"}, 
-                Prefix = "",
-				EntitySpawner = {
-                    {
-                        name = "Medical Box",
-                        entityname = "medic_box",
-                        description = "Used to let your soldiers heal themselves!",
-                        type = "Medic Box"
-                    }
-                },
-            },
-		}
     },
     ["Ecclesiarchy"] = {
         NiceName = "Ecclesiarchy",
